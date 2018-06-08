91a6f619ffd0c004bd8d634fcecdce5e176f7d63
lilycampbell <lilycatcampbell@gmail.com>
2018-06-08 19:30:34 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,10 +1,10 @@
-class People
+class Person
   def initialize(name)
     @name = name 
   end
 end
- adele_goldberg = People.new("adele_goldberg")
- alan_kay = People.new("alan_kay")
+ adele_goldberg = Person.new("adele_goldberg")
+ alan_kay = Person.new("alan_kay")
  
  
  
