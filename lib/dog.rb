d8a5a751e3706b3e30ef696c1313ef451a937cd9
lilycampbell <lilycatcampbell@gmail.com>
2018-06-08 19:14:56 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,17 @@
-class Dog
+class Dog(fido, snoopy, lassie)
   @fido = fido
   @snoopy = snoopy 
   @lassie = lassie
 end 
+
+def fido
+  @fido = fido
+end
+
+def snoopy
+  @snoopy = snoopy
+end
+
+def lassie
+  @lassie
+end
